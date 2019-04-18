67de1735c6f6ddcf372290253f162695efa23ab7
Martin Bravo <bravomartin@gmail.com>
2019-04-18 13:42:37 -0400

    convert

--- convert.sh
+++ convert.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-bayer=scale=480:-1,crop=480:160,hue=s=-10:b=2,pad=1280:768:0:0:black,paletteuse=dither=bayer:bayer_scale=0
-bayer2=scale=480:-1,crop=480:160,hue=s=-10:b=2,paletteuse=dither=bayer:bayer_scale=2
 sierra=scale=480:-1,crop=480:160,pad=1280:768:0:0:black,paletteuse=dither=sierra2
 sierra2=scale=480:-1,crop=480:160,pad=1280:768:0:0:black,paletteuse=dither=sierra2_4a
 
+bayer=scale=480:-1,crop=480:160,hue=s=-10:b=2,pad=1280:768:0:0:black,paletteuse=dither=bayer:bayer_scale=0
+bayer2=scale=480:-1,crop=480:160,hue=s=-10:b=2,paletteuse=dither=bayer:bayer_scale=2
 bayerstack=hstack=inputs=3,scale=-1:160,crop=480:160,hue=b=-1,paletteuse=dither=bayer:bayer_scale=0
+
+
 
 
 for file in $1/*.mp4
