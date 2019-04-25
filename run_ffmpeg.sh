7f030b5bd49b151627ce9baaf2799e6bf628bb9a
Rune Madsen <rune@runemadsen.com>
2019-04-25 13:24:51 +0200

    more

--- run_ffmpeg.sh
+++ run_ffmpeg.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# To test:
-# 1. Watch the ffmpeg output and tweak -preset to be the right-most preset below
-#    where the encoder is keeping up with our desired output frame rate.
-#    Presets: ultrafast, superfast, veryfast, faster, fast, medium
-# 2. Tweak -maxrate if the video quality looks bad. The same with -bufsize
-
-# More info can be found here:
-# https://trac.ffmpeg.org/wiki/EncodingForStreamingSites
-# https://trac.ffmpeg.org/wiki/StreamingGuide
-
 filter=scale=480:-1,crop=480:160:0:0,paletteuse=dither=bayer:bayer_scale=0
 
 while :
